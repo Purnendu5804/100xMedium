bb4896bb16e07d2790041767262dca7669743c77
Purnendu Tiwari <purnendutiwari2004@gmail.com>
2025-07-20 11:20:24 +0530

    add DB initialisation files

---
+++ backend/src/index.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono'
+
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL : string;
+  }
+}>()
+
+app.post("/api/v1/user/signup" , (c)=> {
+
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  return c.text("signup route")
+})
+
+app.post("/api/v1/user/signin" , (c) => {
+  return c.text("signin route")
+})
+
+app.get("/api/v1/blog/:id" , (c) => {
+  const id = c.req.param("id")
+  console.log(id);
+  return c.text("get blog route")
+})
+
+app.post("/api/v1/blog" , (c) => {
+  return c.text("Hello Hono")
+})
+
+app.put("/api/v1/blog" ,(c) => {
+  return c.text("hello hono")
+})
+
+app.get("/api/v1/blog/bulk" , (c) => {
+  return c.text("Hello Hono")
+})
+
+export default app
