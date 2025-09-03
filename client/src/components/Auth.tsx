b260af14f2adcec35cd806a742d9b9d97ea334cc
Purnendu Tiwari <purnendutiwari2004@gmail.com>
2025-09-03 16:17:12 +0530

    add client folder

---
+++ client/src/components/Auth.tsx
@@ -0,0 +1,8 @@
+export const Auth = ({type} : {type : "signup" | "signin"}) => {
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="text-3xl font-extrabold">
+            Create an account
+        </div>
+
+    </div>
+}
